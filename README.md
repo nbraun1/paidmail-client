559b470f96b8a855ffd70b43194b923020c9f115
Nico Braun <n-braun1@gmx.de>
2024-09-07 17:16:27 +0200

    Add Paidmail Client code

---
+++ README.md
@@ -0,0 +1,44 @@
+# Paidmail Client
+
+Simple Python script to retrieve [Paidmails (link to German Wikipedia article)](https://de.wikipedia.org/wiki/Paidmail) from the mailbox and click on the paid link in the mail. Optionally, each Paidmail can be deleted automatically.
+
+Test environment:
+
+- Windows 11
+- Python 3.12.5
+- Gmail
+
+## Supported Paidmailer
+
+If you know of a reputable Paidmailer that I should add to this script, please let me know! :)
+
+- [dondino.de](https://dondino.de)
+
+## Prerequisites
+
+- Python
+- Pip
+- Optional, but recommended: Python [virtual environments](https://docs.python.org/3/library/venv.html)
+
+## Run the Script
+
+1. Clone this repository
+2. Navigate to the root directory of the project
+3. Copy [standard.config.ini](./examples/standard.config.ini) to `config.ini` and adapt the configuration to your requirements (see the section [configuration](#configuration))
+4. Run `pip install -r requirements.txt`
+5. Run `python run.py`
+
+## Configuration
+
+Examples of configurations can be found in the ini files in [this](./examples/) folder.
+
+| **Property**  | **Description**                                                                                                                                                                         | **Default** |
+|---------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|-------------|
+| host          | Imap server hostname                                                                                                                                                                    |             |
+| port          | Imap server port                                                                                                                                                                        | 993         |
+| user          | Your username                                                                                                                                                                           |             |
+| pass          | Your password                                                                                                                                                                           |             |
+| mailbox       | Mailbox from which the Paidmails are read                                                                                                                                               | INBOX       |
+| auto_prune    | Whether the Paidmails found should be deleted automatically                                                                                                                             | false       |
+| trash_mailbox | Mailbox in which the deleted mails are saved and should be removed if `auto_prune` is `true`. In Gmail, for example, this is “[Gmail]/Trash”                                            |             |
+| headless      | Links in found Paidmails are opened in a browser with Selenium. If `headless` is `true`, the browser will **not** open in a GUI. Useful for devices that are not connected to a monitor | false       |
---
+++ run.py
@@ -0,0 +1,133 @@
+import argparse
+import configparser
+import email
+import imaplib
+import logging
+import os
+import re
+import time
+
+from selenium import webdriver
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "-c",
+        "--config-file",
+        dest="config_file",
+        default="config.ini",
+        help='Absolute or relative path to the configuration file. Must be an ".ini" format',
+    )
+    args = arg_parser.parse_args()
+
+    config_file = args.config_file
+    if not os.path.exists(config_file):
+        logger.error(f"Config file {config_file} not exists")
+        exit(1)
+
+    logger.info(f"Read config file {config_file}")
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+
+    for section in config_parser.sections():
+        logger.info(f"Process section {section}")
+        host = config_parser.get(section, "host")
+        port = config_parser.getint(section, "port", fallback=993)
+        user = config_parser.get(section, "user")
+        passw = config_parser.get(section, "pass")
+        mailbox = config_parser.get(section, "mailbox", fallback="INBOX")
+        auto_prune = config_parser.getboolean(section, "auto_prune", fallback=False)
+        trash_mailbox = config_parser.get(section, "trash_mailbox")
+        headless = config_parser.get(section, "headless", fallback=False)
+
+        logger.info(f"Connect to IMAP server {host}:{port}")
+        imap = imaplib.IMAP4_SSL(host, port)
+        imap.login(user, passw)
+
+        logger.info(f"Select mailbox {mailbox} and search for dondino mails")
+        imap.select(mailbox)
+        _, messages = imap.search(None, '(FROM "info@dondino.de")')
+        ids = messages[0].split()
+        logger.info(f"Found {len(ids)} dondino mails")
+
+        links = {}
+        for id in ids:
+            _, data = imap.fetch(id, "(RFC822.TEXT)")
+            _, byte_data = data[0]
+            email_message = email.message_from_bytes(byte_data)
+            for email_part in email_message.walk():
+                content_type = email_part.get_content_type()
+                if content_type == "text/plain" or content_type == "text/html":
+                    content_byte = email_part.get_payload(decode=True)
+                    content = content_byte.decode()
+                    link_matcher = re.search(r"https://dondino\.de/link/\?\w+", content)
+                    if link_matcher:
+                        link = link_matcher.group(0)
+                        links[id.decode()] = link
+                        logger.info(f"Found dondino paid link {link}")
+                    else:
+                        if auto_prune:
+                            _mark_mail_as_deleted(imap, id)
+
+        if links:
+            logger.info(f"Found {len(links)} dondino paid links")
+            if len(ids) != len(links):
+                logger.info(
+                    f"There are some dondino mails that may be “Bonus” or “Info” mails and do not contain paid links"
+                )
+
+            options = webdriver.ChromeOptions()
+            options.add_argument("--no-first-run")
+            options.add_argument("--no-default-browser-check")
+            options.add_argument("--disable-search-engine-choice-screen")
+            if headless:
+                options.add_argument("--headless")
+
+            driver = webdriver.Chrome(options=options)
+            driver.get("https://dondino.de")
+
+            for id, link in links.items():
+                logger.info(f"Open dondino paid link {link}")
+                driver.execute_script(f"window.open('{link}');")
+                if auto_prune:
+                    _mark_mail_as_deleted(imap, id)
+
+            logger.info(
+                "Wait 2 minutes before closing the browser to ensure that all dondino pages are fully loaded"
+            )
+            time.sleep(120)
+            driver.quit()
+
+            if auto_prune:
+                logger.info("Delete all found dondino mails")
+                if _is_gmail_host(imap):
+                    imap.select(mailbox=trash_mailbox)
+                    imap.store("1:*", "+FLAGS", r"(\Deleted)")
+                imap.expunge()
+        else:
+            logger.info(f"No dondino paid links found")
+
+        logger.info("Disconnect from IMAP server")
+        imap.close()
+        imap.logout()
+
+
+def _mark_mail_as_deleted(imap, id):
+    if _is_gmail_host(imap):
+        imap.store(id, "+X-GM-LABELS", r"(\Trash)")
+    else:
+        imap.store(id, "+FLAGS", r"(\Deleted)")
+
+
+def _is_gmail_host(imap):
+    return imap.host.endswith("gmail.com")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+    )
+    main()
